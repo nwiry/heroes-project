9c396ff375cc2fd6d73ff5ade19bdb54d2190f5b
Diogo Cerqueira <diogodeveloper@hotmail.com>
2021-12-06 16:06:02 -0300

    Primeiro Commit

---
+++ src/test/java/com/dioproject/heroesapi/HeroesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dioproject.heroesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
