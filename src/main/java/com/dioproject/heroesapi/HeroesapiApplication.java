03908afc04165438b4f82a47bdd310de7fb64ccc
Diogo Cerqueira <diogodeveloper@hotmail.com>
2021-12-07 15:57:04 -0300

    DynamoDB fixed

--- src/main/java/com/dioproject/heroesapi/HeroesapiApplication.java
+++ src/main/java/com/dioproject/heroesapi/HeroesapiApplication.java
@@ -1,9 +1,11 @@
 package com.dioproject.heroesapi;
 
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDynamoDBRepositories
 public class HeroesapiApplication {
 
 	public static void main(String[] args) {
